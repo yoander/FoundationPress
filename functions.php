8ac1c1f7a37f8650edab30239f0e039b98191ab8
Josh Rathke <josh.rathke@ywammontana.org>
2015-12-13 15:11:12 -0700

    Merge remote-tracking branch 'olefredrik/master'
    # Conflicts:
    #	functions.php

--- functions.php
+++ functions.php
@@ -42,8 +42,13 @@ require_once( 'library/theme-support.php' );
 /** Add Nav Options to Customer */
 require_once( 'library/custom-nav.php' );
 
+<<<<<<< HEAD
 /** Add Custom Shortcodes to Editor */
 require( 'library/editor-shortcodes/shortcodes-settings.php' );
+=======
+/** Change WP's sticky post class */
+require_once( 'library/sticky-posts.php' );
+>>>>>>> olefredrik/master
 
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
