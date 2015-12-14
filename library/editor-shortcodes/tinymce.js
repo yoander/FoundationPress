da4c0ee6033d23e2b11e875fe6302ffbce98a6b6
colin-marshall <colin.michael.marshall@gmail.com>
2015-12-14 16:24:12 -0700

    Added pop-up window for column shortcodes

--- library/editor-shortcodes/tinymce.js
+++ library/editor-shortcodes/tinymce.js
@@ -1,4 +1,16 @@
 (function() {
+
+   // Set to the number of columns in your layout
+   var numCols = 12;
+
+   // Populate column values
+   var colValues = [];
+
+   for (var i = 0; i <= numCols; i++) {
+      colValues.push({text: i.toString(), value: i.toString()})
+   }
+
+   // TinyMCE functionality
    tinymce.PluginManager.add('foundationpress_shortcodes', function(editor, url) {
       editor.addButton('foundationpress_shortcodes', {
          title: 'FoundationPress',
@@ -22,7 +34,50 @@
                      value: '[fdn-col][/fdn-col]',
                      onclick: function(e) {
                         e.stopPropagation();
-                        editor.insertContent(this.value());
+                        editor.windowManager.open( {
+                           title: 'Insert Columns',
+                           body: [
+                             {
+                                 type: 'listbox',
+                                 name: 'smlCol',
+                                 label: 'Small Columns',
+                                 values: colValues
+                              },
+                              {
+                                 type: 'listbox',
+                                 name: 'medCol',
+                                 label: 'Medium Columns',
+                                 values: colValues
+                              },
+                              {
+                                  type: 'listbox',
+                                  name: 'lrgCol',
+                                  label: 'Large Columns',
+                                  values: colValues
+                               }
+                           ],
+                           onsubmit: function(e) {
+                              // Build the column shortcode string
+                              var colString = '[fdn-col';
+
+                              if (e.data.smlCol != '0') {
+                                 colString += ' sml="' + e.data.smlCol + '"';
+                              }
+
+                              if (e.data.medCol != '0') {
+                                 colString += ' med="' + e.data.medCol + '"';
+                              }
+
+                              if (e.data.lrgCol != '0') {
+                                 colString += ' lrg="' + e.data.lrgCol + '"';
+                              }
+
+                              colString += '][/fdn-col]';
+
+                              // Print shortcode string to the editor
+                              editor.insertContent(colString);
+                           }
+                        });
                      }
                   }
                ] // Grid Submenu 
