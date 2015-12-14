1e1edd07b06898b84b7002f4f62576034365f536
colin-marshall <colin.michael.marshall@gmail.com>
2015-12-14 16:42:42 -0700

    Added options for centering columns to pop-up

--- library/editor-shortcodes/tinymce.js
+++ library/editor-shortcodes/tinymce.js
@@ -3,9 +3,10 @@
    // Set to the number of columns in your layout
    var numCols = 12;
 
-   // Populate column values
+   // Create empty array for column values
    var colValues = [];
 
+   // Populate column values array
    for (var i = 0; i <= numCols; i++) {
       colValues.push({text: i.toString(), value: i.toString()})
    }
@@ -50,28 +51,62 @@
                                  values: colValues
                               },
                               {
-                                  type: 'listbox',
-                                  name: 'lrgCol',
-                                  label: 'Large Columns',
-                                  values: colValues
-                               }
+                                 type: 'listbox',
+                                 name: 'lrgCol',
+                                 label: 'Large Columns',
+                                 values: colValues
+                              },
+                              {
+                                 type: 'checkbox',
+                                 name: 'smlCtr',
+                                 label: 'Center Small',
+                                 value: false
+                              },
+                              {
+                                 type: 'checkbox',
+                                 name: 'medCtr',
+                                 label: 'Center Medium',
+                                 value: false
+                              },
+                              {
+                                 type: 'checkbox',
+                                 name: 'lrgCtr',
+                                 label: 'Center Large',
+                                 value: false
+                              }
                            ],
                            onsubmit: function(e) {
                               // Build the column shortcode string
                               var colString = '[fdn-col';
 
-                              if (e.data.smlCol != '0') {
+                              // Small columns
+                              if (e.data.smlCol !== '0') {
                                  colString += ' sml="' + e.data.smlCol + '"';
                               }
 
-                              if (e.data.medCol != '0') {
+                              if (e.data.smlCtr === true) {
+                                 colString += ' sml-ctr';
+                              }
+
+                              // Medium columns
+                              if (e.data.medCol !== '0') {
                                  colString += ' med="' + e.data.medCol + '"';
                               }
 
-                              if (e.data.lrgCol != '0') {
+                              if (e.data.medCtr === true) {
+                                 colString += ' med-ctr';
+                              }
+
+                              // Large columns
+                              if (e.data.lrgCol !== '0') {
                                  colString += ' lrg="' + e.data.lrgCol + '"';
                               }
 
+                              if (e.data.lrgCtr === true) {
+                                 colString += ' lrg-ctr';
+                              }
+
+                              // Close column shortcode
                               colString += '][/fdn-col]';
 
                               // Print shortcode string to the editor
